a66db5696894e126993ce53c70a0df49e8b36d83
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-03-20 11:51:24 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,11 +1,11 @@
 
-l1 = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
-l2 = ['a','e','i','o','u','A','E','I','O','U']
+a1 = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+a2 = ['a','e','i','o','u','A','E','I','O','U']
 
 ch = str(input())
-if (ch in l1):
+if (ch in a1):
     print ("Consonant")
-elif (ch in l2):
+elif (ch in a2):
     print ("Vowel")
 else:
     print("invalid")
