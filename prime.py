93970d34d98485906963bace6ebf22907f1d397e
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-03-20 14:10:47 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -11,8 +11,7 @@ if num > 1:
    # check for factors
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
+           print("no")
            break
    else:
        print("yes")
